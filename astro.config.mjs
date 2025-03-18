eca25a1e0499ea04b0145110f26cf242c46ecbd4
matias2r <matias.espinozamo@gmail.com>
2025-03-18 19:35:49 -0300

    fix: github page config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  site: 'https://matias2r.github.io',
+  base: '/portfolio-matias2r', 
   devToolbar: {
     enabled: false, // Deshabilita el Dev Toolbar
   },
